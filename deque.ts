7c934c57fe73949845a472a5c6d19622e003daef
vadim <pldsrt@gmail.com>
2024-07-16 22:20:33 +0300

    queue

--- deque.ts
+++ deque.ts
@@ -1,108 +1,140 @@
 abstract class ParentQueue<T> {
-  public REMOVE_FRONT_NIL = 0; // remove_front() не вызывалась
-  public REMOVE_FRONT_OK = 1; // последняя операция remove_front() отработала нормально
-  public REMOVE_FRONT_ERR = 2; // список пуст
+    public REMOVE_FRONT_NIL = 0; // remove_front() не вызывалась
+    public REMOVE_FRONT_OK = 1; // последняя операция remove_front() отработала нормально
+    public REMOVE_FRONT_ERR = 2; // список пуст
 
-  public GET_FRONT_NIL = 0; // get_front() не вызывалась
-  public GET_FRONT_OK = 1; // последняя операция get_front() отработала нормально
-  public GET_FRONT_ERR = 2; // список пуст
+    public GET_FRONT_NIL = 0; // get_front() не вызывалась
+    public GET_FRONT_OK = 1; // последняя операция get_front() отработала нормально
+    public GET_FRONT_ERR = 2; // список пуст
 
-  protected _queue: T[];
+    protected _queue: T[];
 
-  private remove_front_status: number; // статус запроса remove_front()
-  private get_front_status: number; // статус запроса get_front()
+    private remove_front_status: number; // статус запроса remove_front()
+    private get_front_status: number; // статус запроса get_front()
 
-  // Конструктор
-  // постусловие: создана пустая очередь
-  constructor() {
-    this._queue = [];
-  }
+    // Конструктор
+    // постусловие: создана пустая очередь
+    constructor() {
+        this._queue = [];
+    }
 
-  // команды
-  // постусловие: в хвост очереди добавлен новый элемент
-  abstract add_tail(value: T): void;
+    // команды
+    // постусловие: в хвост очереди добавлен новый элемент
+    abstract add_tail(value: T): void {
+        this._queue.append(value);
+    }
 
-  // предусловие: очередь не пуста;
-  // постусловие: из головы очереди удалён элемент
-  abstract remove_front(): void;
+    // предусловие: очередь не пуста;
+    // постусловие: из головы очереди удалён элемент
+    abstract remove_front(): void {
+        if (this.size() > 0) {
+            this.remove_front_status = REMOVE_FRONT_OK;
+            return this._queue.remove[0];
+        } else {
+            this.remove_front_status = REMOVE_FRONT_ERR;
+        }
+    }
 
-  // постусловие: из очереди удаляются все элементы
-  abstract clear(): void {
-    this._queue = [];
+    // постусловие: из очереди удаляются все элементы
+    abstract clear(): void {
+        this._queue = [];
 
-    this.remove_front_status = REMOVE_FRONT_NIL;
-    this.get_front_status = GET_FRONT_NIL;
-  };
+        this.remove_front_status = REMOVE_FRONT_NIL;
+        this.get_front_status = GET_FRONT_NIL;
+    };
 
-  // запросы
-  // предусловие: очередь не пуста
-  abstract get_front(): T; // получить элемент из головы очереди;
+    // запросы
+    // предусловие: очередь не пуста
+    abstract get_front(): T { // получить элемент из головы очереди;
+        if (this.size() > 0) {
+            this.get_front_status = GET_FRONT_OK;
+            return this._queue[0];
+        } else {
+            this.get_front_status = GET_FRONT_ERR;
+        }
+    };
 
-  abstract size(): number { // текущий размер очереди
-    return this._queue.length;
-  }
+    abstract size(): number { // текущий размер очереди
+        return this._queue.length;
+    }
 
-  // запросы статусов
-  abstract get_remove_front_status(): number { // успешно; очередь пуста
-    return this.remove_front_status;
-  }
+    // запросы статусов
+    abstract get_remove_front_status(): number { // успешно; очередь пуста
+        return this.remove_front_status;
+    }
 
-  abstract get_get_front_status(): number { // успешно; очередь пуста
-    return this.get_front_status;
-  }
+    abstract get_get_front_status(): number { // успешно; очередь пуста
+        return this.get_front_status;
+    }
 }
 
 abstract class Queue<T> extends ParentQueue<T> {
-  // Конструктор
-  // постусловие: создана пустая очередь
-  constructor() {
-    super();
-  }
+    // Конструктор
+    // постусловие: создана пустая очередь
+    constructor() {
+        super();
+    }
 }
 
 abstract class Deque<T> extends ParentQueue<T> {
-  public REMOVE_TAIL_NIL = 0; // remove_tail() не вызывалась
-  public REMOVE_TAIL_OK = 1; // последняя операция remove_tail() отработала нормально
-  public REMOVE_TAIL_ERR = 2; // список пуст
+    public REMOVE_TAIL_NIL = 0; // remove_tail() не вызывалась
+    public REMOVE_TAIL_OK = 1; // последняя операция remove_tail() отработала нормально
+    public REMOVE_TAIL_ERR = 2; // список пуст
 
-  public GET_TAIL_NIL = 0; // get_tail() не вызывалась
-  public GET_TAIL_OK = 1; // последняя операция get_tail() отработала нормально
-  public GET_TAIL_ERR = 2; // список пуст
+    public GET_TAIL_NIL = 0; // get_tail() не вызывалась
+    public GET_TAIL_OK = 1; // последняя операция get_tail() отработала нормально
+    public GET_TAIL_ERR = 2; // список пуст
 
-  private remove_tail_status: number; // статус запроса remove_tail()
-  private get_tail_status: number; // статус запроса get_tail()
+    private remove_tail_status: number; // статус запроса remove_tail()
+    private get_tail_status: number; // статус запроса get_tail()
 
-  // Конструктор
-  // постусловие: создана пустая очередь
-  constructor() {
-    super();
-  }
+    // Конструктор
+    // постусловие: создана пустая очередь
+    constructor() {
+        super();
+    }
 
-  // Команды
-  // постусловие: в голову очереди добавлен новый элемент
-  abstract add_front(value: T): void;
+    // Команды
+    // постусловие: в голову очереди добавлен новый элемент
+    abstract add_front(value: T): void {
+        this._queue.unshift(value);
+    }
 
-  // предусловие: очередь не пуста;
-  // постусловие: из хвоста очереди удалён элемент
-  abstract remove_tail(): void;
+    // предусловие: очередь не пуста;
+    // постусловие: из хвоста очереди удалён элемент
+    abstract remove_tail(): void {
+        if (this.size() > 0) {
+            this.remove_tail_status = REMOVE_TAIL_OK;
+            return this._queue.remove[this.size() - 1];
+        } else {
+            this.remove_tail_status = REMOVE_TAIL_ERR;
+        }
+    }
 
-  abstract clear() {
-    super.clear();
+    abstract clear() {
+        super.clear();
 
-    this.remove_tail_status = REMOVE_TAIL_NIL;
-    this.get_tail_status = GET_TAIL_NIL;
-  }
+        this.remove_tail_status = REMOVE_TAIL_NIL;
+        this.get_tail_status = GET_TAIL_NIL;
+    }
 
-  // Запросы
-  // предусловие: очередь не пуста
-  abstract get_tail(): T; // получить элемент из хвоста очереди;
+    // Запросы
+    // предусловие: очередь не пуста
+    abstract get_tail(): T { // получить элемент из хвоста очереди;
+        if (this.size() > 0) {
+            this.get_tail_status = GET_TAIL_OK;
+            return this._queue[this.size() - 1];
+        } else {
+            this.get_tail_status = GET_TAIL_ERR;
+        }
+    }
 
-  // запросы статусов
-  abstract get_remove_tail_status(): number { // успешно; очередь пуста
-    return this.remove_tail_status;
-  }
+    // запросы статусов
+    abstract get_remove_tail_status(): number { // успешно; очередь пуста
+        return this.remove_tail_status;
+    }
 
-  abstract get_get_tail_status(): number { // успешно; очередь пуста
-    return this.get_tail_status;
-  }
+    abstract get_get_tail_status(): number { // успешно; очередь пуста
+        return this.get_tail_status;
+    }
 }
