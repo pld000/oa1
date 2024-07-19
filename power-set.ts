387ffd9b9dac14a21c5f0602b46df36cfa6eaaba
pld000 <pld000@yandex.ru>
2024-07-19 17:43:25 +0300

    power-set

--- power-set.ts
+++ power-set.ts
@@ -3,14 +3,9 @@ abstract class PowerSet {
   public REMOVE_OK = 1; // последняя операция remove() отработала нормально
   public REMOVE_ERR = 2; // множество пустое
 
-  public GET_NIL = 0; // операция get() не вызывалась
-  public GET_OK = 1; // последняя операция get() отработала нормально
-  public GET_ERR = 2; // множество пустое
-
   private poserSet: any[];
 
   private remove_status: number; // статус запроса remove()
-  private get_status: number; // статус запроса get()
 
   // Конструктор
   // постусловие: создано пустое множество
@@ -24,40 +19,28 @@ abstract class PowerSet {
     this.poserSet.push(value);
   }
 
-  // предусловие: в таблице есть элемент с ключем key
-  // постусловие: из словаря удален элемент с ключем key
-  abstract remove(key: string): void {
-    if (this.isKey(key)) {
+  // предусловие: значение value присутствует во множестве
+  // постусловие: из множества удалено значение value
+  abstract remove(value: any): void {
+    if (this.get(value)) {
       this.remove_status = this.REMOVE_OK;
-      const slot = this.slots[hashFn(key)];
-      this.slots[slot] = null;
-      this.values[slot] = null;
+      this.poserSet.remove(value);
     } else {
-      this.get_status = this.REMOVE_ERR;
+      this.remove_status = this.REMOVE_ERR;
     }
   }
 
   // постусловие: из словаря удаляются все элементы
   abstract clear(): void {
     this.poserSet = [];
-
     this.remove_status = this.REMOVE_NIL;
-    this.get_status = this.GET_NIL;
   };
 
   // Запросы
-  // предусловие: в таблице есть элемент с ключем key
-  abstract get(key: string): T { // получить значение по ключу key
-    if (this.isKey(key)) {
-      this.get_status = this.GET_OK;
-      const slot = this.slots[hashFn(key)];
-      return this.values[slot];
-    } else {
-      this.get_status = this.GET_ERR;
-    }
+  abstract get(value: any): boolean { // проверить наличие значения во множестве
+    return Boolean(this.poserSet.find(item => item === value));
   }
 
   // Запросы статусов
   abstract get_remove_status() // элемент удален; в словаре нет элемента с ключем key
-  abstract get_get_status() // элемент получен; в словаре нет элемента с ключем key
 }
